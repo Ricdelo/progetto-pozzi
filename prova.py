5307cf0cd6264584c051ebffc3d38c45f1ecba43
Ricdelo <riccardo.giacomo18@gmail.com>
2024-05-31 21:52:08 +0200

    creazione della base

--- prova.py
+++ prova.py
@@ -2,13 +2,13 @@ from telegram.ext import Updater, CommandHandler
 
 # Funzione per gestire il comando /start
 def start(update, context):
-    update.message.reply_text('ciao')
+    update.message.reply_text('Ciao!')
 
 def main():
-    # Inserisci qui il tuo token
+    # Inserisci qui il token del tuo bot
     token = '7340572849:AAFERqqSCr5shXnejBgIcpdExFTni8tAPaI'
 
-    # Crea l'Updater e il Dispatcher
+    # Crea l'Updater con il token del bot
     updater = Updater(token)
 
     # Ottieni il dispatcher per registrare i gestori di comando
@@ -17,7 +17,7 @@ def main():
     # Registra il gestore per il comando /start
     dispatcher.add_handler(CommandHandler('start', start))
 
-    # Avvia il Bot
+    # Avvia il bot
     updater.start_polling()
 
     # Mantieni il bot in esecuzione fino a quando non viene interrotto
