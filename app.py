b8bb51759e1d6e1290cea54e958b71d926d39984
Ricdelo <riccardo.giacomo18@gmail.com>
2024-06-02 14:14:42 +0200

    aggiunta callback per i bottoni, creazione del file config, TODO: richiedere l ID all'API e calcolare i trofei

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
 import messaggi
+import config
 
-BOT_TOKEN = "7340572849:AAFERqqSCr5shXnejBgIcpdExFTni8tAPaI"
-API_TOKEN="5e4ce336b55348c4bb710f846eb4e5af"
+BOT_TOKEN = config.TOKEN_BOT
+API_TOKEN=config.TOKEN_API
 
 # Crea un'istanza dell'oggetto Bot
 bot = telebot.TeleBot(BOT_TOKEN)
 
 def scelta_competionze():
-    # Crea la tastiera con i bottoni
+    # Crea la tastiera con i bottoni con le top 5 cinque competizioni
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     button1 = telebot.types.KeyboardButton("🏴󠁧󠁢󠁥󠁮󠁧󠁿Premier League")
     button2 = telebot.types.KeyboardButton("🇮🇹Seria A")
@@ -20,18 +22,23 @@ def scelta_competionze():
     markup.add(button3,button4,button5)
     return markup
 
+def get_trofei(ogg):
+    None
+    
+    
+
 #funzione per resituore le squadre di una competizione
 def competizione(comp):
     base_url = 'https://api.football-data.org/v4/competitions'
     
     url = f'{base_url}/{comp}/teams'
     headers = {'X-Auth-Token': API_TOKEN}
-    params = {'season': "2022"}
+    params = {'season': "2022"} #stagione 2022
     
     response = requests.get(url, headers=headers, params=params)
     
     if response.status_code == 200:
-        # Se la richiesta è stata fatta con successo, processare i dati
+        #se richiesta va a buon fine salva tutti i team in una lista
         data = response.json()
         team_names = [team['name'] for team in data['teams']]
         return team_names
@@ -44,19 +51,16 @@ def competizione(comp):
 def crea_bottoni(comp):
     markup = telebot.types.InlineKeyboardMarkup()
     for team in competizione(comp):
-        btn = telebot.types.InlineKeyboardButton(team, callback_data="btn")
+        btn = telebot.types.InlineKeyboardButton(team, callback_data=team)
         markup.add(btn)
     return markup
 
 #funzione per gestire le callback dei bottoni
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    if call.data == "btn1":
-        bot.answer_callback_query(call.id, "Hai premuto Bottone 1")
-    elif call.data == "btn2":
-        bot.answer_callback_query(call.id, "Hai premuto Bottone 2")
-    elif call.data == "btn3":
-        bot.answer_callback_query(call.id, "Hai premuto Bottone 3")   
+    get_trofei(call.data)
+    bot.answer_callback_query(call.id, f"You selected {call.data}")
+    #bot.answer_callback_query(call.id, "Hai premuto Bottone ")   
 
 
 
@@ -73,7 +77,7 @@ def start_command(message):
 def option_selected(message):
 
     selected_option = message.text
-     
+    #quando viene scelta una competizione viene passato l ID a crea_bottoni()
     if selected_option == "🏴󠁧󠁢󠁥󠁮󠁧󠁿Premier League":
         bot.send_message(message.chat.id, messaggi.mss_competizione(), reply_markup=crea_bottoni("PL"))            
     elif selected_option == "🇮🇹Seria A":
